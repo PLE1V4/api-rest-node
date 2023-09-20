5eab017c2a64da01d817d77a1bb61d1ba2cefffb
Pablo Leiva <96198232+PLE1V4@users.noreply.github.com>
2023-09-20 16:10:26 -0300

    Update index.js
    initiliazed cors, and server listener

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const {con} = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
+
 
 //Inicializar APP
 console.log("App Started");
@@ -7,3 +10,18 @@ console.log("App Started");
 con();
 
 //Crear Node Server
+const app = express();
+const port = 7777;
+
+//Configurar cors
+app.use(cors());
+
+//BODY => JSON
+app.use(express.json());
+
+//ROUTES
+
+//crear servidor y escuchar peticiones http
+app.listen(port, () => {
+    console.log("Server listening at port "+port);
+});
