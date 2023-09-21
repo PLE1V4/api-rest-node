c0d65345e04b645d368e1206f0d32f220a04e2f4
Pablo Leiva <96198232+PLE1V4@users.noreply.github.com>
2023-09-21 00:35:11 -0300

    IMG & Search
    Added Search and image upload

--- controllers/article.js
+++ controllers/article.js
@@ -1,5 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
 const Article = require("../models/Article");
 const {validateArticle} = require("../helpers/validate");
+const { error } = require("console");
 
 const create = (req, res) => {
 
@@ -159,10 +163,107 @@ const edit = (req,res) => {
 
 }
 
+const upload = (req, res) => {
+
+    if(!req.file && !req.files){
+        return res.status(404).json({
+            status: "error",
+            message: "Invalid Request"
+        });
+    }
+
+    let fileName = req.file.originalname;
+    let split_file = fileName.split("\.");
+    let extension = split_file[1];
+
+    if(extension != "png" &&  extension != "jpg" && extension != "jpeg" && extension != "gif"){
+        fs.unlink(req.file.path, (error) =>{
+            return res.status(400).json({
+                status: "error",
+                message: "Unsupported File Format"
+            });
+        });
+    }else{
+        
+        let id = req.params.id;
+          
+        Article.findOneAndUpdate({_id: id}, {img: req.file.filename }, {new:true}).then((newArticle) => {
+            if(!newArticle){
+                return res.status(404).json({
+                    status: 'error',
+                    mensaje: "Article Not Found"
+                }); 
+            }
+
+            return res.status(200).json({
+                status: "success",
+                article: newArticle
+            });
+        }).catch((error) =>{
+            return res.status(500).json({
+                status: 'error',
+                mensaje: "Unexpected Error"
+            });
+        });
+
+    }
+   
+}
+
+const image = (req, res) => {
+    let file = req.params.file;
+    let file_path = "./img/articles/"+file;
+
+    fs.stat(file_path, (error, exist) => {
+        if(exist){
+            return res.sendFile(path.resolve(file_path));
+        }else{
+            return res.status(404).json({
+                status: "error",
+                message: "File Not Found"
+            });
+        }
+    });
+}
+
+const search = (req, res) => {
+
+    let lookup = req.params.query;
+
+    Article.find({ "$or": [
+        {"title": { "$regex": lookup, "$options": "i"}},
+        {"content": { "$regex": lookup, "$options": "i"}}
+    ]})
+    .sort({date: -1})
+    .then((foundArticles) => {
+        if(!foundArticles || foundArticles.length <= 0){
+            return res.status(404).json({
+                status: "error",
+                message: "Cant find Articles meeting the criteria"
+            });
+        }
+
+        return res.status(200).json({
+            status: "success",
+            articles: foundArticles
+            })
+    })
+    .catch((error) => {
+        return res.status(400).json({
+            status: "error",
+            message: "Unexpected Error"
+        });
+    });
+
+}
+
 module.exports = {
     create,
     getArticles,
     one,
     del,
-    edit
+    edit,
+    upload,
+    image,
+    search
 }
--- routes/article.js
+++ routes/article.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 
 const ArticleController = require("../controllers/article");
 
+const localStorage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null,'./img/articles/');
+    },
+    filename: function(req, file, cb){
+        cb(null, "article"+Date.now()+file.originalname);
+    }
+});
+
+const uploads = multer({storage: localStorage});
+
 router.post("/create", ArticleController.create);
+router.post('/upload-image/:id',[uploads.single("file")],ArticleController.upload);
+
 router.get("/articles/:recent?",ArticleController.getArticles);
 router.get("/article/:id",ArticleController.one);
+router.get("/image/:file",ArticleController.image)
+router.get("/search/:query",ArticleController.search);
+
 router.delete("/article/:id",ArticleController.del);
+
 router.put("/article/:id",ArticleController.edit);
+
 
 
 module.exports = router;
